13be87442dc9a8274d27392381e9fffca68059d6
DjonniStorm <i.pazushkin@yandex.ru>
2024-03-29 02:12:27 +0400

    уже как-то

--- index.js
+++ index.js
@@ -1,22 +1,35 @@
-const $btnLeft  = document.querySelector('#left-div')
-const $btnRight = document.querySelector('#right-div')
+document.querySelector('a').addEventListener('click', (event) => event.preventDefault())
 const $slides   = document.querySelectorAll('.js-slide')
-let nowSlide = 0
-function moveSlides(direction) {
+let nowSlide    = 0
+function direction(direction) {
     switch(direction) {
         case '+1':
             nowSlide++
+            if (nowSlide >= $slides.length) {
+                nowSlide = $slides.length - 1
+            }
+            slider()
             break
         case '-1':
             nowSlide--
+            if (nowSlide < 0) {
+                nowSlide = 0
+            }
+            slider()
             break
         default:
             break
     }
-    if (nowSlide >= $slides.length) nowSlide = 0
-    if (nowSlide < 0) nowSlide = $slides.length - 1
-    for (let i = 0; i < $slides.length; i++) {
+}
+function slider() {
+    const $btnLeft  = document.getElementById('left')
+    const $btnRight = document.getElementById('right')
+    for (let i = 0, length = $slides.length; i < length; i++) {
         $slides[i].classList.add('none')
     }
     $slides[nowSlide].classList.remove('none')
+    if (nowSlide === 0 && !$btnLeft.classList.contains('none')) $btnLeft.classList.add('none')
+    else if ($btnLeft.classList.contains('none') && nowSlide !== 0) $btnLeft.classList.remove('none')
+    if (nowSlide === $slides.length - 1 && !$btnRight.classList.contains('none')) $btnRight.classList.add('none')
+    else if ($btnRight.classList.contains('none') && nowSlide !== $slides.length - 1) $btnRight.classList.remove('none')
 }
