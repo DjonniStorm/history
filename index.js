7fd0f69da8ec5e23303ea0a2c942af9e4156828b
DjonniStorm <i.pazushkin@yandex.ru>
2024-04-01 00:49:06 +0400

    ep

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 document.querySelector('a').addEventListener('click', (event) => event.preventDefault())
 const $slides   = document.querySelectorAll('.js-slide')
 let nowSlide    = 0
+//а теперь двигаем блоки в таймлайне
+const $timelineCentures = document.querySelectorAll('.century-content')
 function direction(direction) {
     switch(direction) {
         case '+1':
@@ -27,7 +29,15 @@ function slider() {
     for (let i = 0, length = $slides.length; i < length; i++) {
         $slides[i].classList.add('none')
     }
+    $slides[nowSlide].style.opacity = '0'
     $slides[nowSlide].classList.remove('none')
+    $timelineCentures[nowSlide].scrollIntoView({ block: "center", behavior: "smooth" })
+    let counter = 1
+    let interval1 = setInterval(() => {
+        $slides[nowSlide].style.opacity = (0.2 * counter).toString()
+        counter++
+        if (counter >= 6) clearInterval(interval1)
+    }, 100)
     if (nowSlide === 0 && !$btnLeft.classList.contains('none')) $btnLeft.classList.add('none')
     else if ($btnLeft.classList.contains('none') && nowSlide !== 0) $btnLeft.classList.remove('none')
     if (nowSlide === $slides.length - 1 && !$btnRight.classList.contains('none')) $btnRight.classList.add('none')
